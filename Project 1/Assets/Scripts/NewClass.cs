620338d44d0d3a02b5b4ccc6bc5ecb4f564170bf
Joshua Baum <jbaum447@gmail.com>
2020-01-15 10:51:13 -0700

    Variable Lecture

--- Project 1/Assets/Scripts/NewClass.cs
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class NewClass : MonoBehaviour
 {
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
     // Start is called before the first frame update
-    void Start()
+   private void Start()
     {
-        print(message:"Hello World");
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
     }
 
     // Update is called once per frame
