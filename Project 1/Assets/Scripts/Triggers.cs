8aad30c65eb6259e82d31308f0316860323bafc0
Joshua Baum <jbaum447@gmail.com>
2020-01-28 21:31:18 -0700

    Trigger Events homework

---
+++ Project 1/Assets/Scripts/Triggers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Triggers : MonoBehaviour
+{
+    public UnityEvent triggerExitEvent;
+    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerStayEvent;
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggerStayEvent.Invoke();
+    }
+}
