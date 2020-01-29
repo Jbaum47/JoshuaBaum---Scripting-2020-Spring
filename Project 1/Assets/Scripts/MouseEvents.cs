0b3898e2fd84866ebba9bf7cf046db192d1bad57
Joshua Baum <jbaum447@gmail.com>
2020-01-29 11:20:09 -0700

    Mouse Events

---
+++ Project 1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+    public UnityEvent mouseUpEvent;
+    public UnityEvent mouseDragEvent;
+    public UnityEvent mouseEnterEvent;
+    public UnityEvent mouseExitEvent;
+    public UnityEvent mouseOverEvent;
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+    private void OnMouseDrag()
+    {
+       mouseDragEvent.Invoke();
+    }
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+}
---
+++ Project 1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public float fNumber;
+    public int iNumber;
+    public string sNumber;
+    public UnityEvent TriggerEnters;
+    public UnityEvent TriggerStays;
+    public UnityEvent TriggerExits;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnters.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStays.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerExits.Invoke();
+    }
+}
