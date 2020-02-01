5f292f25d057f69a62a477e484fa1c660ed1a4a5
Joshua Baum <jbaum447@gmail.com>
2020-01-31 22:31:22 -0700

    Did some turtorial Script

---
+++ Project 1/Assets/Scripts/DemoLightScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This Script is from a learning Unity Demo (https://unity3d.com/learning-c-sharp-in-unity-for-beginners)
+public class DemoLightScript : MonoBehaviour
+{
+    public Light myLight;
+
+    private void Awake()
+    {
+        int myVar = AddTwo(9, 2);
+        Debug.Log(myVar);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("Space"))
+        {
+            MyFunction();
+        }
+    }
+
+    void MyFunction()
+    {
+        myLight.enabled = !myLight.enabled;
+    }
+
+    int AddTwo(int var1, int var2)
+    {
+        int retrunValue = var1 + var2;
+        return retrunValue;
+    }
+}
