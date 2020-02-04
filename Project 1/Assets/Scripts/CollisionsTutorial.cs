2397dc3985cf47caca6527363c23eeaaca6e7a77
Joshua Baum <jbaum447@gmail.com>
2020-02-03 21:29:32 -0700

    Went over Collisons at home

---
+++ Project 1/Assets/Scripts/CollisionsTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CollisionsTutorial : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collison)
+    {
+        Debug.Log("Enter called.");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Stay occuring..");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit called..");
+    }
+}
