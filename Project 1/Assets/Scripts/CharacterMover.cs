ee39a561b0343515ab87c2ec9ee58a078ecb6ee3
Joshua Baum <jbaum447@gmail.com>
2020-02-10 11:13:49 -0700

    If Statements, Rigidbodies, scope, components

--- Project 1/Assets/Scripts/CharacterMover.cs
+++ Project 1/Assets/Scripts/CharacterMover.cs
@@ -1,26 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     public Vector3 positionDirection;
     public float speed = 10f;
     public float gravity = 2f;
     public float jumpForce = 30f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
     
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
        
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
         positionDirection.y -= gravity;
         controller.Move(positionDirection*Time.deltaTime);
---
+++ Project 1/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public string password = "OU812";
+    public bool canRun = true;
+    void Start()
+    {
+        if (canRun == true)
+        {
+           print("We can run."); 
+        }
+
+        if (password == "OU812")
+        {
+            print("That is correct.");
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
