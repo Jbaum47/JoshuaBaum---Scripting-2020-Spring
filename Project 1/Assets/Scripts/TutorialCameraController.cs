f2fd4cb97dc1f29d82cd5a70b1995baaa1008754
Joshua Baum <Jbaum447@gmail.com>
2020-02-14 16:06:25 -0700

    Worked on a simple tutorial

---
+++ Project 1/Assets/Scripts/TutorialCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Found this Script at: https://learn.unity.com/project/roll-a-ball-tutorial?courseId=5c61706dedbc2a324a9b022d
+public class TutorialCameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Project 1/Assets/Scripts/TutorialRotater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Found this Script at: https://learn.unity.com/project/roll-a-ball-tutorial?courseId=5c61706dedbc2a324a9b022d
+public class TutorialRotater : MonoBehaviour
+{
+   private void Update()
+   {
+      transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+   }
+}
