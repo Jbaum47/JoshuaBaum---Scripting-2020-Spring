f73cc9f760a8e1b21a4541e4e7e03be71c215c1e
Joshua Baum <jbaum447@gmail.com>
2020-02-19 11:18:09 -0700

    Health and Score lecture.

---
+++ Project 1/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+  private Image imageObj;
+  
+  private void Start()
+  {
+    imageObj = GetComponent<Image>();
+  }
+}
