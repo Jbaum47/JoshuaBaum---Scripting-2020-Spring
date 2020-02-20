2b7bcf6314552b3fa79d7a395d4e8f60b4a5a66d
Joshua Baum <jbaum447@gmail.com>
2020-02-19 22:00:43 -0700

    Worked on health bar a little bit

--- Project 1/Assets/Scripts/ImageBehaviour.cs
+++ Project 1/Assets/Scripts/ImageBehaviour.cs
@@ -8,9 +8,29 @@ using UnityEngine.UI;
 public class ImageBehaviour : MonoBehaviour
 {
   private Image imageObj;
-  
+  public float health;
+
   private void Start()
   {
     imageObj = GetComponent<Image>();
+  }
+
+  public void Update()
+  {
+    imageObj.fillAmount = health;
+    
+    if (imageObj.fillAmount > .5)
+    {
+      imageObj.color = Color.green;
+    }
+    if (imageObj.fillAmount <= .5)
+    {
+      imageObj.color = Color.yellow;
+    }
+
+    if (imageObj.fillAmount <=.25)
+    {
+      imageObj.color = Color.red;
+    }
   }
 }
