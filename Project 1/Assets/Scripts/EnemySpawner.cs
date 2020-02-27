5fc23f5e419b8bcc19ea90d39af74afb352f9bd0
Joshua Baum <jbaum447@gmail.com>
2020-02-26 19:29:20 -0700

    Tried creating an enemy spawning script

---
+++ Project 1/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public IntData enemyCount;
+    public GameObject enemyObj;
+
+    public void Start()
+    {
+        Invoke("SpawnEnemy");
+    }
+
+    public void Update()
+    {
+        if (enemyCount.value >= 6)
+        {
+            
+        }
+    }
+}
