0cfb5bba7ca661dc4b3367bea6eeee6c1f623028
Joshua Baum <jbaum447@gmail.com>
2020-02-27 21:59:07 -0700

    Followed spawn tutorial

---
+++ Project 1/Assets/Scripts/InstantiationAndSpawnerTutorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Found this at: https://www.youtube.com/watch?v=qmNlY4Y68o0
+public class InstantiationAndSpawnerTutorial : MonoBehaviour
+{
+    public GameObject mPrefab;
+    private void Start()
+    {
+        GameObject.Instantiate(mPrefab, transform.position, Quaternion.identity);
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            GameObject.Instantiate(mPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
