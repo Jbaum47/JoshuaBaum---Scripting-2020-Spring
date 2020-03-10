2af2539dd0669c9fa92fb3d04318017c6d678eff
Joshua Baum <jbaum447@gmail.com>
2020-03-09 19:30:09 -0600

    worked on enemy movment

---
+++ Project 1/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        Debug.Log(direction);
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition(transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
