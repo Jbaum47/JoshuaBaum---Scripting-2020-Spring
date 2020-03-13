8f44081bc1f4143d344426ea818672ccd77ece8b
Joshua Baum <jbaum447@gmail.com>
2020-03-12 22:27:02 -0600

    worked on collections script

--- Project 1/Assets/Scripts/CollectableList.cs
+++ Project 1/Assets/Scripts/CollectableList.cs
@@ -6,6 +6,34 @@ using UnityEngine;
 public class CollectableList : ScriptableObject
 {
     public List<Collectable> collectables;
-    
-    
+    public Collectable collectableItem;
+
+    public void AddToCollectableList(Collectable collectableObj)
+    {
+        collectables.Add(collectableObj);
+    }
+
+    public void RemoveFromCollectableList()
+    {
+        for (int i = 0; i < collectables.Count; i++)
+        {
+            if (collectables[i] == collectableObj)
+            {
+                collectables.Remove(collectableObj);
+            } 
+        }
+    }
+
+    public void CheckList()
+    {
+        foreach (var obj in collectables)
+        {
+            if (obj == collectableItem)
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+
+
 }
