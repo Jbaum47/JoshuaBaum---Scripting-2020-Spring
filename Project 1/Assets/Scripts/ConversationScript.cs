df10c88ff4feafbe24ed9bd127984f34d63ee125
Joshua Baum <jbaum447@gmail.com>
2020-03-24 23:31:06 -0600

    Followed a tutorial on switches

---
+++ Project 1/Assets/Scripts/ConversationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationScript : MonoBehaviour
+{
+  public int intelligence = 5;
+
+  void Greet()
+  {
+    switch (intelligence)
+    {
+      case 5:
+        print("Why hello there good sir! Let me teach ou about Trigonometry!");
+        break;
+      case 4:
+        print("");
+        break;
+      case 3:
+        print("");
+        break;
+      case 2:
+        print("");
+        break;
+      case 1:
+        print("");
+        break;
+      default:
+        print("");
+        break;
+    }
+  }
+}
