8a29dd04bd1a7074ddc37cdb81626ad5e78cb2f6
Joshua Baum <jbaum447@gmail.com>
2020-03-26 23:03:07 -0600

    Tried to create script to change scene... Didnt really work

---
+++ Project 1/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        
+        }
+}
