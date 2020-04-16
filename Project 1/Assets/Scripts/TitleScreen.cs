3e69f1e9f6a79cd3ec7653bb08e2243146e31bb4
Joshua Baum <jbaum447@gmail.com>
2020-04-15 22:31:50 -0600

    Worked on start screen

---
+++ Project 1/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+   public string newGameScene;
+
+   public void NewGame()
+   {
+      SceneManager.LoadScene(newGameScene);
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+   }
+
+}
