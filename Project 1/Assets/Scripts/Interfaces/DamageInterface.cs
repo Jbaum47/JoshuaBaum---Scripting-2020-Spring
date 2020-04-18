8f63077482c1f50f1f310ead6282a6421d2a7ca7
Joshua Baum <jbaum447@gmail.com>
2020-04-17 23:13:34 -0600

    Worked on interfaces

---
+++ Project 1/Assets/Scripts/Interfaces/DamageInterface.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Idamageable<T>
+{
+    //Must implement into every new script ( add , after inherited class)
+    void Damage(T damageAmount);
+}
