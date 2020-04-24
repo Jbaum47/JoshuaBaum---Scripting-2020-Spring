2d56b218bf1a38c6147c7b4ef2a4ac19ce9a069f
Joshua Baum <jbaum447@gmail.com>
2020-04-23 23:50:53 -0600

    Class lesson

---
+++ Project 1/Assets/Scripts/SimpleCoroutine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleCoroutine : MonoBehaviour
+{
+    public float seconds = 1f;
+    private WaitForSeconds waitObj;
+    public bool canRun = true;
+    public UnityEvent repeatEvent;
+
+    private void Awake()
+    {
+        waitObj = new WaitForSeconds(seconds);
+    }
+
+    public void Reset()
+    {
+        StartCoroutine(OnStartCoroutine());
+    }
+
+    public IEnumerator OnStartCoroutine()
+    {
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return waitObj;
+        }
+    }
+}
